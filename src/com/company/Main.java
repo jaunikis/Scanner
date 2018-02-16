7f75f200289ca1bd16eadd1c460831736d575d19
clonesstores <jaunikis@gmail.com>
2018-02-16 16:35:35 +0000

    baigtas

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("enter digit: ");
+        int x=scn.nextInt();
+        System.out.println();
+        System.out.println("you entered "+x);
+    }
+}
